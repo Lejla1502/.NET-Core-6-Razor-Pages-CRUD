ca6afa66b2601a719f37a2c11732bc616222b408
Lejla1502 <lejlasphotmail.com>
2022-04-22 12:22:17 +0200

    adding create form

---
+++ AbbyWeb/Pages/Categories/Index.cshtml.cs
@@ -0,0 +1,21 @@
+using AbbyWeb.Data;
+using AbbyWeb.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AbbyWeb.Pages.Categories
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ApplicationDbContext _ctx;
+        public IEnumerable<Category> Categories { get; set; }   
+        public IndexModel(ApplicationDbContext ctx)
+        {
+            _ctx = ctx;
+        }
+        public void OnGet()
+        {
+            Categories = _ctx.Category;
+        }
+    }
+}
