37ca2be20f6903c4d142bb09ef3365cd74ffb5b8
Lejla1502 <lejlasphotmail.com>
2022-04-22 12:45:52 +0200

    adding post handler

--- AbbyWeb/Pages/Categories/Create.cshtml.cs
+++ AbbyWeb/Pages/Categories/Create.cshtml.cs
@@ -1,14 +1,31 @@
+using AbbyWeb.Data;
 using AbbyWeb.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace AbbyWeb.Pages.Categories
 {
+    [BindProperties]
     public class CreateModel : PageModel
     {
+        private readonly ApplicationDbContext _ctx;
         public Category Category { get; set; }
+        public CreateModel(ApplicationDbContext ctx)
+        {
+            _ctx = ctx;
+        }
         public void OnGet()
         {
+
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            await _ctx.Category.AddAsync(Category);
+            await _ctx.SaveChangesAsync();
+
+            return RedirectToPage("Index");
+
         }
     }
 }
